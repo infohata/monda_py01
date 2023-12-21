3f41524028b2c54abf230a17a56bf1e7e42e6ffa
Orionyx <zanstas1@yahoo.com>
2023-12-21 14:48:13 +0300

    Darbas12/21/2023

---
+++ 01_programming_basics/1Assingment.py
@@ -0,0 +1,8 @@
+fst_number = int(input("Give me the first number: "))
+scnd_number = int(input("Give me the second number: "))
+print(f"Alright, so we're working with {fst_number} & {scnd_number}")
+operation = (input("""What type of equation are we going for?
++, -, *, or /: """))
+if operation == "+":
+    result = fst_number + scnd_number
+    print(f"{fst_number} + {scnd_number} is  = {result}")
